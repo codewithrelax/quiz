a2bb720b154522728f8cf25a62c3758d5fda9773
codewithrelax <138682266+codewithrelax@users.noreply.github.com>
2023-07-05 18:27:02 +0530

    admin.py

--- quizsystem/quiz/admin.py
+++ quizsystem/quiz/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_name', 'option1','option2','option3','option4',correct_option')
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer')
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+
